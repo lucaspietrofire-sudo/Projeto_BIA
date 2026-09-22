2c010d7f55972fee7659a3486f354b0cea05e09f
Lucas Pietro <seu_email_do_github@exemplo.com>
2026-09-22 18:41:04 -0300

    refactor: atualiza app.py

--- app.py
+++ app.py
@@ -11,35 +11,37 @@ def conectar():
 
 def enviar_evento():
     conexao, channel = conectar()
+
+    # Garante que a fila continue existindo mesmo se o RabbitMQ reiniciar
     channel.queue_declare(queue='fila_bia', durable=True)
     
-    evento = {"projeto": "BIA", "status": "Processar dados com IA"}
+    evento = {"projeto": "BIA", "status": "Gerar relatorio"}
     
     channel.basic_publish(
         exchange='',
         routing_key='fila_bia',
         body=json.dumps(evento),
-        properties=pika.BasicProperties(delivery_mode=2)
+        properties=pika.BasicProperties(delivery_mode=2)    # delivery_mode=2 torna a mensagem persistente no disco
     )
-    print(" -> [PRODUTOR] Evento enviado para a fila!")
+    print("[PRODUTOR] Evento enviado para a fila!")
     conexao.close()
 
 def iniciar_consumidor():
     conexao, channel = conectar()
     channel.queue_declare(queue='fila_bia', durable=True)
     
-    print(' [*] Aguardando mensagens. Para simular que o sistema CAIU, aperte CTRL+C.')
+    print('Aguardando mensagens. Para simular que o sistema CAIU, aperte CTRL+C.')
 
     def callback(ch, method, properties, body):
         dados = json.loads(body)
-        print(f" <- [CONSUMIDOR] Evento recebido: {dados['status']}")
+        print(f"[CONSUMIDOR] Evento recebido: {dados['status']}")
         ch.basic_ack(delivery_tag=method.delivery_tag)
 
     channel.basic_consume(queue='fila_bia', on_message_callback=callback)
     try:
         channel.start_consuming()
     except KeyboardInterrupt:
-        print("\n [!] O consumidor foi DESLIGADO (Simulação de falha).")
+        print("\nO consumidor foi DESLIGADO (Simulação de falha).")
         conexao.close()
 
 if __name__ == '__main__':
